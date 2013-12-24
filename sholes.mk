b18778721079b83496c5e124ff8d86b91a836131
Noah Greene <chevycam94@gmail.com>
2013-12-24 02:35:27 -0500

    Several enhancements, now made official
    Change-Id: Ic7facdaaf0ec5f53be930c04de5bff381d6ac8c2

--- sholes.mk
+++ sholes.mk
@@ -32,23 +32,22 @@ $(call inherit-product-if-exists, vendor/motorola/sholes/sholes-vendor.mk)
 
 ## (3)  Finally, the least specific parts, i.e. the non-CDMA-specific aspects
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.com.android.wifi-watchlist=GoogleGuest \
-	ro.error.receiver.system.apps=com.google.android.feedback \
-	ro.setupwizard.enterprise_mode=1 \
-	ro.com.google.clientidbase=android-verizon \
-	ro.com.google.locationfeatures=1 \
-	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-	ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-	ro.cdma.home.operator.numeric=310004 \
-	ro.cdma.home.operator.alpha=Verizon \
-	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
-	ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
-	ro.config.vc_call_vol_steps=7 \
-	ro.telephony.call_ring.multiple=false \
-	ro.telephony.call_ring.delay=3000 \
-	ro.url.safetylegal=http://www.motorola.com/staticfiles/Support/legal/?model=A855 \
-	ro.setupwizard.enable_bypass=1 \
-	ro.media.dec.jpeg.memcap=20000000
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.google.clientidbase=android-verizon \
+    ro.com.google.locationfeatures=0 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.cdma.home.operator.numeric=310004 \
+    ro.cdma.home.operator.alpha=Verizon \
+    ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+    ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
+    ro.config.vc_call_vol_steps=7 \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.call_ring.delay=3000 \
+    ro.url.safetylegal=http://www.motorola.com/staticfiles/Support/legal/?model=A855 \
+    ro.setupwizard.enable_bypass=1 \
+    ro.media.dec.jpeg.memcap=20000000
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/sholes/overlay
 
